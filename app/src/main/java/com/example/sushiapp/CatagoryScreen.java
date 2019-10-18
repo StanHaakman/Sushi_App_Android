b8ce1c9f99bb2426c23403af27825c7cb597ade7
Stan Haakman <stan@Stans-MacBook-Pro.local>
2019-10-18 13:39:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sushiapp/CatagoryScreen.java
@@ -0,0 +1,19 @@
+package com.example.sushiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridLayout;
+
+public class CatagoryScreen extends AppCompatActivity {
+
+    GridLayout mainGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catagory_screen);
+
+//        mainGrid = (GridLayout)findViewById(R.id.);
+    }
+}
